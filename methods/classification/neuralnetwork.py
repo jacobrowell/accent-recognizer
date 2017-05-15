de8d67985bd7bb80a27579f41bd02cac010c39ba
jacobrowell <jacobrowell000@gmail.com>
2017-05-15 22:47:50 +0300

    added NNClassificator train() method

--- methods/classification/neuralnetwork.py
+++ methods/classification/neuralnetwork.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain2.tools.customxml.networkwriter import NetworkWriter
 from pybrain2.tools.customxml.networkreader import NetworkReader
+from pybrain.structure import TanhLayer
+import json
+from time import time
 
 
 class NNClassifier:
@@ -17,6 +24,27 @@ class NNClassifier:
             "JA",
             "KO",
         ]
+
+    def train(self):
+        samples = open("models/samples.json", "r")
+        accents = json.load(samples)
+
+        net = buildNetwork(13, 3, 7, hiddenclass=TanhLayer)
+
+        ds = SupervisedDataSet(13, 7)
+
+        for accent in accents:
+            samples = accents[accent]["samples"]
+            out = accents[accent]["out"]
+
+            for sample in samples:
+                ds.addSample(sample, out)
+
+        trainer = BackpropTrainer(net)
+        trainer.trainOnDataset(ds, 10000)
+        trainer.testOnData(ds, verbose=True)
+
+        NetworkWriter.writeToFile(net, 'models/model-{}.xml'.format(int(time())))
 
     def classify(self, cache_file):
         x = []

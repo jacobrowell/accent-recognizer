c4b07eb37d82a4551eefd7faedd1a24e615895bf
jacobrowell <jacobrowell000@gmail.com>
2017-06-27 23:36:05 +0300

    removed memory bottleneck at mfcc calculation logic

--- prepare_data/prepare_mfcc.py
+++ prepare_data/prepare_mfcc.py
@@ -1,9 +1,8 @@
 import os
-import glob
+from glob import glob
 import json
 import scipy.io.wavfile
 from scikits.talkbox.features import mfcc
-from time import time
 
 
 def prepare_mfcc(dirs):
@@ -12,43 +11,33 @@ def prepare_mfcc(dirs):
     :return: None
     """
 
-    accents = dict()
-
     for d in dirs:
         for speaker in os.listdir(d):
             accent_name = speaker.split('_')[0]
             if len(accent_name) > 2:
                 continue
 
-            if accent_name not in accents:
-                accents[accent_name] = dict()
-                accents[accent_name]["samples"] = list()
-
-            wav_names = glob.glob('{}/{}/part*.wav'.format(d, speaker))
+            wav_names = glob('{}/{}/part*.wav'.format(d, speaker))
 
             for name in wav_names:
+                sample = {
+                    "accent": accent_name,
+                    "speaker": speaker
+                }
+                part_name = name.split('/')[-1].split('.')[0]
+
                 try:
-                    sample_rate, X = scipy.io.wavfile.read(name)
-                    ceps, mspec, spec = mfcc(X)
-                    accents[accent_name]["samples"].append(ceps.tolist())
+                    sample_rate, x = scipy.io.wavfile.read(name)
+                    ceps, mspec, spec = mfcc(x)
+                    sample["ceps"] = ceps.tolist()
+
+                    try:
+                        os.mkdir("samples/{}".format(speaker))
+                    except OSError:
+                        pass
+
+                    save_file = open("samples/{}/{}.json".format(speaker, part_name), "w")
+                    json.dump(sample, save_file, indent=4)
+                    save_file.close()
                 except ValueError:
                     print "error reading {}".format(name)
-                except:
-                    print "some unexpected error"  # TODO: remove handling here
-
-            # FIXME: this causes memory overflow
-            save_file = open("samples/samples_tmp.json", "w")
-            json.dump(accents, save_file, indent=4)
-            save_file.close()
-
-    num_accents = len(accents)
-    i = 0
-
-    for accent in accents:
-        accents[accent]["out"] = [0] * num_accents
-        accents[accent]["out"][i] = 1
-        i += 1
-
-    save_file = open("samples/samples-{}.json".format(int(time())), "w")
-    json.dump(accents, save_file, indent=4)
-    save_file.close()

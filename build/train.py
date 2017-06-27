b20f2c9708b64b64564f7c17f19b4967459a388f
jacobrowell <jacobrowell000@gmail.com>
2017-06-28 01:07:09 +0300

    rewrote training for the new file structure; added it to main

--- build/train.py
+++ build/train.py
@@ -1,32 +1,42 @@
+import os
+import json
+import logging
+from time import time
+from glob import glob
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain2.tools.customxml.networkwriter import NetworkWriter
 from pybrain.structure import TanhLayer
-import json
-from time import time
 
 
 def train():
-    samples = open("samples.json", "r")  # REVIEW: possibly out of memory here
-    accents = json.load(samples)
-
+    logging.basicConfig(filename="logfile.log", level=logging.DEBUG)
+    accents = ['CH', 'EN', 'IN', 'IR', 'IT', 'JA', 'KO']
     net = buildNetwork(13, 3, 7, hiddenclass=TanhLayer)
-
     ds = SupervisedDataSet(13, 7)
 
-    for accent in accents:
-        samples = accents[accent]["samples"]
-        out = accents[accent]["out"]
+    logging.info("Started training network")
 
-        for sample in samples:
-            ds.addSample(sample, out)
+    try:
+        for speaker in os.listdir('samples'):
+            samples = glob('{}/part*'.format(speaker))
+            for sample in samples:
+                s = json.load(open(sample))
+                out = [0, 0, 0, 0, 0, 0, 0]
+                out[accents.index(s['accent'])] = 1
+                ds.addSample(s['ceps'], out)
 
-    trainer = BackpropTrainer(net)
-    trainer.trainOnDataset(ds, 10000)
-    trainer.testOnData(ds, verbose=True)
+        trainer = BackpropTrainer(net)
+        trainer.trainOnDataset(ds, 10000)
+        trainer.testOnData(ds, verbose=True)
 
-    NetworkWriter.writeToFile(net, 'models/model-{}.xml'.format(int(time())))
+        NetworkWriter.writeToFile(net, 'models/model-{}.xml'.format(int(time())))
+    except Exception as e:
+        msg = "Something bad happened: {}".format(e.message)
+        print msg
+        logging.error(msg)
+
 
 if __name__ == "main":
     train()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from prepare_data.move_files import move_files
 from prepare_data.cut_speech import cut_speech
 from prepare_data.prepare_mfcc import prepare_mfcc
+from build.train import train
 
 # TODO: accept path to files as program parameter
 
@@ -13,3 +14,4 @@ dirs = [
 move_files(dirs)
 cut_speech(dirs)
 prepare_mfcc(dirs)
+train()

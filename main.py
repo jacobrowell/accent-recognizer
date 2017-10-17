4f7608ee4482e77af26acfb8e7de6e71b4a071eb
jacobrowell <jacobrowell000@gmail.com>
2017-10-17 21:05:56 +0300

    added control using command line arguments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
 
+from prepare_data.prepare_mfcc import prepare_mfcc
 from prepare_data.move_files import move_files
 from prepare_data.cut_speech import cut_speech
-from prepare_data.prepare_mfcc import prepare_mfcc
+from classify.classify import classify
 from build.train import train
+import sys
 
 # TODO: accept path to files as program parameter
 
@@ -11,7 +13,33 @@ dirs = [
     '/home/eugen/student/diploma/code/c20/audioFiles/task80'
 ]
 
-move_files(dirs)
-cut_speech(dirs)
-prepare_mfcc(dirs)
-train()
+action = sys.argv[1]
+
+# if not action:
+#     move_files(dirs)
+#     cut_speech(dirs)
+#     prepare_mfcc(dirs)
+#     train()
+if action == 'prepare':
+    move_files(dirs)
+    cut_speech(dirs)
+    prepare_mfcc(dirs)
+elif action == 'build':
+    train()
+elif action == 'classify':
+    filename = sys.argv[2]
+    if not filename or type(filename) != str:
+        filename = input('Provide a path to audio file: ')
+    res = classify(filename)
+    print res
+elif action == 'help':
+    print ''.join([
+        'This is a program to classify speaker accent using neural network.\n\n',
+        'Supply such arguments:\n',
+        'prepare - to prepare data for training\n',
+        'build - to build and train network\n',
+        'classify [filename] - to classify the provided speech sample\n',
+        'help - show this message and exit',
+    ])
+else:
+    print 'Unknown action'
